190b22b619f6afd2791df19c20227b0a8ac64ea0
Łukasz <lukasz.p.gorny@gmail.com>
2018-10-02 10:52:34 +0200

    secondaaa

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+var first = [
+	'Święty',
+	'Pijany',
+	'Upalony',
+	'Opasły'
+];
+
+var second = [
+	'Borsuk',
+	'Świnia'
+	'Jeż'
+];
+
+
 $(document).ready(function() {
 	$("#randomize").on('click', getRandomNickname)
 });
 
 var getRandomNickname = function() {
-	$("#nick").append('lalala');
+	var firstRandom = $.randomBetween(0, first.length);
+	var secondRandom = $.randomBetween(0, second.length);
+	var firstRandomWord = first[firstRandom];
+	var secondRandomWord = second[secondRandom];
+	
+	$("#nick").append(firstRandomWord + ' ' + secondRandomWord);
 };
